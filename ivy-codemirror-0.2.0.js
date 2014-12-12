0a901cdb2d712ab8b8c20e387a916dede543f203
Dray Lacy <dray@envylabs.com>
2014-12-12 18:29:12 -0500

    Release v0.2.0.

--- ivy-codemirror-0.2.0.js
+++ ivy-codemirror-0.2.0.js
@@ -1,9 +1,121 @@
-define(
+(function() {;
+var define, requireModule, require, requirejs;
+
+(function() {
+
+  var _isArray;
+  if (!Array.isArray) {
+    _isArray = function (x) {
+      return Object.prototype.toString.call(x) === "[object Array]";
+    };
+  } else {
+    _isArray = Array.isArray;
+  }
+  
+  var registry = {}, seen = {}, state = {};
+  var FAILED = false;
+
+  define = function(name, deps, callback) {
+  
+    if (!_isArray(deps)) {
+      callback = deps;
+      deps     =  [];
+    }
+  
+    registry[name] = {
+      deps: deps,
+      callback: callback
+    };
+  };
+
+  function reify(deps, name, seen) {
+    var length = deps.length;
+    var reified = new Array(length);
+    var dep;
+    var exports;
+
+    for (var i = 0, l = length; i < l; i++) {
+      dep = deps[i];
+      if (dep === 'exports') {
+        exports = reified[i] = seen;
+      } else {
+        reified[i] = require(resolve(dep, name));
+      }
+    }
+
+    return {
+      deps: reified,
+      exports: exports
+    };
+  }
+
+  requirejs = require = requireModule = function(name) {
+    if (state[name] !== FAILED &&
+        seen.hasOwnProperty(name)) {
+      return seen[name];
+    }
+
+    if (!registry[name]) {
+      throw new Error('Could not find module ' + name);
+    }
+
+    var mod = registry[name];
+    var reified;
+    var module;
+    var loaded = false;
+
+    seen[name] = { }; // placeholder for run-time cycles
+
+    try {
+      reified = reify(mod.deps, name, seen[name]);
+      module = mod.callback.apply(this, reified.deps);
+      loaded = true;
+    } finally {
+      if (!loaded) {
+        state[name] = FAILED;
+      }
+    }
+
+    return reified.exports ? seen[name] : (seen[name] = module);
+  };
+
+  function resolve(child, name) {
+    if (child.charAt(0) !== '.') { return child; }
+
+    var parts = child.split('/');
+    var nameParts = name.split('/');
+    var parentBase;
+
+    if (nameParts.length === 1) {
+      parentBase = nameParts;
+    } else {
+      parentBase = nameParts.slice(0, -1);
+    }
+
+    for (var i = 0, l = parts.length; i < l; i++) {
+      var part = parts[i];
+
+      if (part === '..') { parentBase.pop(); }
+      else if (part === '.') { continue; }
+      else { parentBase.push(part); }
+    }
+
+    return parentBase.join('/');
+  }
+
+  requirejs.entries = requirejs._eak_seen = registry;
+  requirejs.clear = function(){
+    requirejs.entries = requirejs._eak_seen = registry = {};
+    seen = state = {};
+  };
+})();
+
+;define("ivy-codemirror/components/ivy-codemirror",
   ["codemirror","ember","exports"],
   function(__dependency1__, __dependency2__, __exports__) {
     "use strict";
-    var CodeMirror = __dependency1__["default"] || __dependency1__;
-    var Ember = __dependency2__["default"] || __dependency2__;
+    var CodeMirror = __dependency1__["default"];
+    var Ember = __dependency2__["default"];
 
     __exports__["default"] = Ember.Component.extend({
       /**
@@ -159,3 +271,50 @@ define(
       }
     });
   });
+;define("ivy-codemirror/index",
+  ["ivy-codemirror/components/ivy-codemirror","exports"],
+  function(__dependency1__, __exports__) {
+    "use strict";
+    var IvyCodemirrorComponent = __dependency1__["default"];
+
+    __exports__.IvyCodemirrorComponent = IvyCodemirrorComponent;
+  });
+;/* global define, require */
+define('ivy-codemirror-shim', ['exports'], function(__exports__) {
+  'use strict';
+  __exports__['default'] = function(container) {
+    container.register('component:ivy-codemirror', require('ivy-codemirror/components/ivy-codemirror')['default']);
+  };
+});
+;/* global define, require, window */
+var addonName = 'ivy-codemirror';
+
+define('codemirror', ['exports'], function(__exports__) {
+  __exports__['default'] = window.CodeMirror;
+});
+
+define('ember', ['exports'], function(__exports__) {
+  __exports__['default'] = window.Ember;
+});
+
+var index = addonName + '/index';
+define(addonName, ['exports'], function(__exports__) {
+  var library = require(index);
+  Object.keys(library).forEach(function(key) {
+    __exports__[key] = library[key];
+  });
+});
+
+// Glue library to a global var
+window.IvyCodemirror = require(index);
+
+// Register library items in the container
+var shim = addonName + '-shim';
+window.Ember.Application.initializer({
+  name: shim,
+
+  initialize: function(container) {
+    require(shim).initialize(container);
+  }
+});
+})();
