f6d172089592cab7f064d4359e7ca20127a04b57
Dray Lacy <dray@envylabs.com>
2014-12-15 11:02:47 -0500

    Release v0.2.1.

--- ivy-codemirror-0.2.1.js
+++ ivy-codemirror-0.2.1.js
@@ -314,7 +314,7 @@ window.Ember.Application.initializer({
   name: shim,
 
   initialize: function(container) {
-    require(shim).initialize(container);
+    require(shim)['default'](container);
   }
 });
 })();
